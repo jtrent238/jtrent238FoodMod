3ce6ea9c91ebf59132c7a2caff1f7d9d236d1cb6
jtrent238 <jtrent238@outlook.com>
2015-11-30 18:51:42 -0500

    Prerelease 1.0.0.0

---
+++ src/main/java/com/jtrent238/weaponmod/Blockruby.java
@@ -0,0 +1,12 @@
+package com.jtrent238.weaponmod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class Blockruby extends Block {
+
+	protected Blockruby(Material material) {
+		super(material);
+	}
+
+}
---
+++ src/main/java/com/jtrent238/weaponmod/Itembiggoldsword.java
@@ -0,0 +1,7 @@
+package com.jtrent238.weaponmod;
+
+import net.minecraft.item.Item;
+
+public class Itembiggoldsword extends Item {
+
+}
