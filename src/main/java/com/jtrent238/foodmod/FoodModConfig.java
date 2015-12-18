7f06bf4083a526e8fbe359704d60555dea53c67e
Trent Patterson <jtrent238@outlook.com>
2015-12-18 14:23:21 -0500

    Update and rename FoodModConfig to FoodModConfig.java

--- src/main/java/com/jtrent238/foodmod/FoodModConfig.java
+++ src/main/java/com/jtrent238/foodmod/FoodModConfig.java
@@ -6,16 +6,17 @@ import net.minecraftforge.common.Property;
 
 public class FoodModConfig
 {
-  static int ChocoCakeID;
-  static int AppleCakeID;
-  static int CarrotCakeID;
-  static int MagicCakeID;
-  static int BChocolateID;
-  static int caramelCakeID;
+  static int itemchocolatecakeID;
+  static int itemapplecakeID;
+  static int itemcarrotcakeID;
+  static int itemmagiccakeID;
+  static int blockchocolateID;
+  static int itemcaramelcakeID;
   public static int AppleCakeItemID;
   public static int ChocoCakeItemID;
   public static int CarrotCakeItemID;
   public static int CaramelCakeItemID;
+  public static int itemmilkbottleID;
   public static Configuration config;
   
   public static void FC_Configuration(FMLPreInitializationEvent event)
@@ -23,12 +24,14 @@ public class FoodModConfig
     config = new Configuration(event.getSuggestedConfigurationFile());
     config.load();
     
-    ChocoCakeID = config.getBlock("Chocolate Cake", 503).getInt();
-    AppleCakeID = config.getBlock("Apple Cake", 504).getInt();
-    CarrotCakeID = config.getBlock("Carrot Cake", 505).getInt();
-    MagicCakeID = config.getBlock("Magic Cake", 506).getInt();
-    BChocolateID = config.getBlock("Chocolate Block", 507).getInt();
-    caramelCakeID = config.getBlock("Caramel Cake", 509).getInt();
+    itemmilkbottleID = config.getItem("Milk Bottle", 4041).getInt();
+    
+    itemchocolatecakeID = config.getBlock("Chocolate Cake", 503).getInt();
+    itemapplecakeID = config.getBlock("Apple Cake", 504).getInt();
+    itemcarrotcakeID = config.getBlock("Carrot Cake", 505).getInt();
+    itemmagiccakeID = config.getBlock("Magic Cake", 506).getInt();
+    blockchocolateID = config.getBlock("Chocolate Block", 507).getInt();
+    itemcaramelcakeID = config.getBlock("Caramel Cake", 509).getInt();
     
     ChocoCakeItemID = config.getItem("ChocoCakeItem", 4016).getInt();
     AppleCakeItemID = config.getItem("AppleCakeItem", 4017).getInt();
